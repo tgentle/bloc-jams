cfb992d78da5ff39025503072e6fb49ef8369715
tgentle <taylormgentle@gmail.com>
2017-02-15 22:16:43 -0500

    assignment-8-animation-revision

--- scripts/landing.js
+++ scripts/landing.js
@@ -15,7 +15,13 @@ var animatePoints = function(points) {
  };
 
 window.onload = function() {
+    //Automatically animate the points on a tall screen where scrolling 
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray); 
+    }
+    
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     window.addEventListener ('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
