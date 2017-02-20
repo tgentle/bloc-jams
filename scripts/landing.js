1fe1cebbd18e9efa04b17bae06977c1b3c944bc7
tgentle <taylormgentle@gmail.com>
2017-02-20 00:55:24 -0500

    assignment-9-events-revised

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-     
-     var revealPoint = function(index){
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
     
-     for (var i=0; i < points.lenght; i++) {
-         revealpoint(i);
-     }
+var revealPoint = function(point){
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+    
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
  };
 
 window.onload = function() {
