26495efa1b5d7f5601a45d126f1c0a9f7b0365f7
tgentle <taylormgentle@gmail.com>
2017-03-15 23:30:42 -0400

    checkpoint-17-jquery-collectionalbum complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,7 @@
-var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
+// #1
+var buildCollectionItemTemplate = function() {
+     var template =
+      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
@@ -13,16 +15,20 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
+    
+// #2
+return $(template);
+};
+   
 
-window.onload = function() {
-    //#1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = ' ';
-    
-    //#3
+$(window).load(function(){
+    // #3
+    var $collectionContainer = $('.album-covers');
+    // #4
+    $collectionContainer.empty();
     for (var i=0; i <12; i++){
-        collectionContainer.innerHTML += collectionItemTemplate;
-    }
-    
-}
+        var $newThumbnail = buildCollectionItemTemplate();
+        // #5
+        $collectionContainer.append($newThumbnail);
+    } 
+});
